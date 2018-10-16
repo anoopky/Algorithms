0adfff710e140d6b9fab895e01a38de60cb70867
anoopky <anoophky1@gmail.com>
2018-10-16 11:57:27 +0530

    adding files

---
+++ Data_Structure/Set.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+using namespace std;
+
+int main(){
+
+	
+    return 0;
+}
---
+++ Data_Structure/Vector.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+using namespace std;
+
+int main(){
+
+	vector<int> v;
+
+	v.pb(5);
+	v.pb(3);
+
+	cout << v[0] << endl;
+
+	for(int i=0; i< v.size(); i++)
+		cout << v[i] << endl;
+
+	for(auto x: v)
+		cout << x << endl;
+
+	vector<int> v1 = {8,9,3,5,1,6,7};
+
+	cout << v1.back() << endl;
+	v1.pop_back();
+	cout << v1.back() << endl;
+
+	sort(v1.begin(), v1.end());
+
+	for(auto x: v1)
+		cout << x << " ";
+	cout << endl;
+
+
+	reverse(v1.begin(), v1.end());
+
+	for(auto x:v1)
+		cout << x << " ";
+	cout << endl;
+
+	random_shuffle(v1.begin(), v1.end());
+
+	for(auto x: v1)
+		cout << x << " ";
+	cout << endl;
+
+	vector<int> v2 = {2,3,3,5,7,8,8,8,9};
+
+	auto a = lb(v2.begin(), v2.end(), 2);
+	auto b = ub(v2.begin(), v2.end(), 3);
+
+	cout << *a << endl;
+	cout << a - v2.begin() << endl; // index
+	cout << *b << endl;
+
+	
+    return 0;
+}
